36aeeaaae77aee92cc60c02c2c41774e553f4e7c
MarouaneManar <marouane.manar@toulouse.miage.fr>
2024-04-03 19:43:01 +0200

    rajout module et component pour page connexion

---
+++ src/app/connexion/connexion.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormulaireConnexionComponent } from './formulaire-connexion/formulaire-connexion.component';
+
+
+
+@NgModule({
+  declarations: [
+    FormulaireConnexionComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ConnexionModule { }
