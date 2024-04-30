1f420e96ccdb7d61c804395d481136155eaed573
anais665 <anais.soutric@toulouse.miage.fr>
2024-04-30 11:10:31 +0200

    .

--- src/app/creation-projet/creation-projet.component.ts
+++ src/app/creation-projet/creation-projet.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProjectService } from '../services/project.service';
 
 @Component({
   selector: 'app-creation-projet',
@@ -6,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './creation-projet.component.css'
 })
 export class CreationProjetComponent {
+
+  constructor(private router: Router, private projectService: ProjectService) { }
 
 }
