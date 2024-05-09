a49286ccd7eaef215d55f5a154a26c7bcdd872e3
Marouane Manar <dev.manar31@gmail.com>
2024-05-09 13:55:05 +0200

    Marouane - ajout clear local storage quand on se déconnecte

--- src/app/commun/nav-bar/nav-bar.component.ts
+++ src/app/commun/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class NavBarComponent {
 
+  constructor(private authService: AuthService) { }
+
+  logout(): void {
+    this.authService.logout(); 
+    localStorage.clear();
+  }
 }
