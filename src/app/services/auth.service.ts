e62c844306433dd2c66bb712c501f047184dee6c
MarouaneManar <marouane.manar@toulouse.miage.fr>
2024-05-14 11:46:46 +0200

    M

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { catchError } from 'rxjs/operators';
 import { utilisateur } from '../models/authentification';
 
 // Injectable permet de declarer un service dans un module
